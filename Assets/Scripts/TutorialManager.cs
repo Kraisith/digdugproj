847ebb5dc08aff5690a049facbddcd69954d0e04
Kraisith <ashley.lau71@yahoo.co.uk>
2021-04-09 12:32:47 +0100

    Updated tutorial with clear condition, redesigned both levels 1 and 2
    Added 1 more tutorial message that lets the player know that all enemies must be killed to progress, redesigned levels 1 and 2 to fit the overall level design template

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -6,7 +6,7 @@ public class TutorialManager : MonoBehaviour
 {
     [SerializeField] private LightBandit lB;
     [SerializeField] private HeavyBandit hB;
-    [SerializeField] private GameObject tut1, tut2, tut3, tut4, tut5, tut6, tut7;
+    [SerializeField] private GameObject tut1, tut2, tut3, tut4, tut5, tut6, tut7, tut8;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,9 +46,14 @@ public class TutorialManager : MonoBehaviour
             tut6.SetActive(false);
             tut7.SetActive(true);
         } 
-        else if (tutNum == 8) //after last tutorial is clicked
+        else if (tutNum == 8) 
         {
             tut7.SetActive(false);
+            tut8.SetActive(true);
+        }
+        else if (tutNum == 9) //after last tutorial is clicked
+        {
+            tut8.SetActive(false);
             lB.setTutorialStatus(false);
             hB.setTutorialStatus(false);
         }
